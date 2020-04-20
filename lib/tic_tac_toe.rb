868e6321e940fc5ebf632599fe03d4c2524a37ec
William Damm <wdamm@powerhrg.com>
2020-04-20 15:54:36 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -96,9 +96,18 @@ if winning_board != nil
 end
 end
 
+
+
+
 def play(board)
   until over?(board) == true
   turn(board)
+end
+if won?(board) == true
+  player = winner(board)
+  puts "Congratulations player #{player}"
+  elsif draw?(board) == true
+  puts "Cat's Game!"
 end
 end
 
